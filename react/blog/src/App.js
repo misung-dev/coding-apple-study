81616c6f240287ded1f37a088244959828b2dee7
misung.dev <misung.dev@gmail.com>
2024-07-01 21:30:48 +0900

    리액트 환경에서 동적인 UI 만드는 법 (모달창만들기)

--- react/blog/src/App.js
+++ react/blog/src/App.js
@@ -4,6 +4,7 @@ import "./App.css";
 function App() {
 	let [글제목, 글제목변경] = useState(["남자코트 추천", "강남 우동맛집", "파이썬 독학"]);
 	let [따봉, 따봉변경] = useState(0);
+	let [modal, setModal] = useState(false);
 
 	return (
 		<div className="App">
@@ -54,9 +55,27 @@ function App() {
 				<p>2월 17일 발행</p>
 			</div>
 			<div className="list">
-				<h4>{글제목[2]}</h4>
+				<h4
+					onClick={() => {
+						setModal(!modal);
+					}}
+				>
+					{글제목[2]}
+				</h4>
 				<p>2월 17일 발행</p>
 			</div>
+
+			{modal == true ? <Modal /> : null}
+		</div>
+	);
+}
+
+function Modal() {
+	return (
+		<div className="modal">
+			<h4>제목</h4>
+			<p>날짜</p>
+			<p>상세내용</p>
 		</div>
 	);
 }
