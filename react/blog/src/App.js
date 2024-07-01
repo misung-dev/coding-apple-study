7ebc7f3884b3eb19fb03a7cc95049adb4fa50160
misung.dev <misung.dev@gmail.com>
2024-07-01 21:45:00 +0900

    좋아요를 누를 때 마다 따봉 갯수를 개별적으로 증가하도록

--- react/blog/src/App.js
+++ react/blog/src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 
 function App() {
 	let [글제목, 글제목변경] = useState(["남자코트 추천", "강남 우동맛집", "파이썬 독학"]);
-	let [따봉, 따봉변경] = useState(0);
+	let [따봉, 따봉변경] = useState([0, 0, 0]); // 각 상태를 별개로 관리
 	let [modal, setModal] = useState(false);
 
 	return (
@@ -12,70 +12,27 @@ function App() {
 				<div>개발 blog</div>
 			</div>
 
-			{/* map을 활용한 축약형 */}
-			{글제목.map(function (a) {
+			{글제목.map(function (a, i) {
 				return (
-					<div className="list">
-						<h4>{a}</h4>
+					<div className="list" key={i}>
+						<h4>
+							{글제목[i]}
+							<span
+								onClick={() => {
+									let copy = [...따봉];
+									copy[i] = copy[i] + 1;
+									따봉변경(copy);
+								}}
+							>
+								👍
+							</span>{" "}
+							{따봉[i]}
+						</h4>
 						<p>2월 18일 발행</p>
+						{modal == true ? <Modal /> : null}
 					</div>
 				);
 			})}
-
-			{/* 글자 정렬 버튼 */}
-			<button
-				onClick={() => {
-					let copy = [...글제목];
-					copy.sort();
-					글제목변경(copy);
-				}}
-			>
-				{" "}
-				정렬버튼{" "}
-			</button>
-
-			{/* 글제목 일부 변경 */}
-			<button
-				onClick={() => {
-					// let copy = 글제목; // 글제목에 있던 화살표가 복사됨
-					let copy = [...글제목];
-					copy[0] = "여자코트 추천"; // array를 수정했지 변수에 있던 화살표는 수정안됨
-					글제목변경(copy);
-				}}
-			>
-				글수정
-			</button>
-
-			<div className="list">
-				<h4>
-					{글제목[0]}
-					<span
-						onClick={() => {
-							따봉변경(따봉 + 1);
-						}}
-					>
-						👍
-					</span>
-					{따봉}
-				</h4>
-				<p>2월 17일 발행</p>
-			</div>
-			<div className="list">
-				<h4>{글제목[1]}</h4>
-				<p>2월 17일 발행</p>
-			</div>
-			<div className="list">
-				<h4
-					onClick={() => {
-						setModal(!modal);
-					}}
-				>
-					{글제목[2]}
-				</h4>
-				<p>2월 17일 발행</p>
-			</div>
-
-			{modal == true ? <Modal /> : null}
 		</div>
 	);
 }
