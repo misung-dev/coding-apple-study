98f99733a7f7017e6006aa0181c917803db31356
misung.dev <misung.dev@gmail.com>
2024-07-02 11:46:43 +0900

    코드 길어지면 import export 하면 됩니다

--- react/shop/src/App.js
+++ react/shop/src/App.js
@@ -1,8 +1,14 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Navbar, Container, Nav } from "react-bootstrap";
+import bg from "./bg.png";
+import { useState } from "react";
+import data from "./data";
 
 function App() {
+	let [shoes] = useState(data);
+	console.log(shoes);
+
 	return (
 		<div>
 			<Navbar bg="dark" variant="dark">
@@ -15,6 +21,26 @@ function App() {
 					</Nav>
 				</Container>
 			</Navbar>
+			<div className="main-bg" style={{ backgroundImage: "url(" + bg + ")" }}></div>\{" "}
+			<div className="container">
+				<div className="row">
+					<div className="col-md-4">
+						<img src="https://codingapple1.github.io/shop/shoes1.jpg" width="80%" />
+						<h4>{shoes[0].title}</h4>
+						<p>상품정보</p>
+					</div>
+					<div className="col-md-4">
+						<img src="https://codingapple1.github.io/shop/shoes2.jpg" width="80%" />
+						<h4>{shoes[1].title}</h4>
+						<p>상품정보</p>
+					</div>
+					<div className="col-md-4">
+						<img src="https://codingapple1.github.io/shop/shoes3.jpg" width="80%" />
+						<h4>{shoes[2].title}</h4>
+						<p>상품정보</p>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 }
